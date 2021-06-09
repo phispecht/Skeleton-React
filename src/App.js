9a95ff07aa61d9024b739c27bdb39e263e5d391f
phispecht <philippspecht1990@gmail.com>
2021-06-10 00:21:47 +0200

    skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "App.css";
+/* import someImage from "./someImage.jpg"; */
+
+const App = () => (
+  <div>
+    <h1 className="title">React</h1>
+    {/* <img src={someImage} alt="" /> */}
+  </div>
+);
+
+export default App;
